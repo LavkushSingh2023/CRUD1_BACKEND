4cabc87f706da21d1aeee49b041648b634ae99aa
Lavkush Singh <t21425814@gmail.com>
2025-06-14 11:22:54 +0530

    last

--- Server.js
+++ Server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require('./routes/userRoutes')
+const path = require("path")
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 
 app.use('/api/users/', userRoutes)
 
